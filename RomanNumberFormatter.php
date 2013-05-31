f01916ff8fce2bc2fb810f470cde3137e08b9780
jeroendedauw <jeroendedauw@gmail.com>
2013-05-31 18:06:51 +0200

    Implemented full solution without error handling

--- RomanNumberFormatter.php
+++ RomanNumberFormatter.php
@@ -2,12 +2,77 @@
 
 class RomanNumberFormatter {
 
-	public function __construct() {
+	protected $symbolMap;
 
+	public function __construct() {
+		$this->symbolMap = array(
+			array( 'I', 'V' ),
+			array( 'X', 'L' ),
+			array( 'C', 'D' ),
+			array( 'M' ),
+		);
 	}
 
-	public function formatNumber() {
-		return '';
+	public function formatNumber( $number ) {
+		$romanNumber = '';
+
+		for ( $i = 0; $i < count( $this->symbolMap ) ; $i++ ) {
+			$divisor = pow( 10, $i + 1 );
+			$remainder = $number % $divisor;
+			$digit = $remainder / pow( 10, $i );
+
+			$number -= $remainder;
+			$romanNumber .= $this->formatDigit( $digit, $i );
+
+			if ( $number === 0 ) {
+				break;
+			}
+		}
+
+		return $romanNumber;
+	}
+
+	protected function formatDigit( $digit, $orderOfMagnitude ) {
+		if ( $digit === 0 ) {
+			return '';
+		}
+
+		if ( $digit === 4 || $digit === 9 ) {
+			return $this->formatFourOrNine( $digit, $orderOfMagnitude );
+		}
+
+		$romanNumber = '';
+
+		if ( $digit >= 5 ) {
+			$digit -= 5;
+			$romanNumber .= $this->getFiveSymbol( $orderOfMagnitude );
+		}
+
+		$romanNumber .= $this->formatOneToThree( $orderOfMagnitude, $digit );
+
+		return $romanNumber;
+	}
+
+	protected function formatFourOrNine( $digit, $orderOfMagnitude ) {
+		$firstSymbol = $this->getOneSymbol( $orderOfMagnitude );
+		$secondSymbol = $digit === 4 ? $this->getFiveSymbol( $orderOfMagnitude ) : $this->getTenSymbol( $orderOfMagnitude );
+		return $firstSymbol . $secondSymbol;
+	}
+
+	protected function formatOneToThree( $orderOfMagnitude, $digit ) {
+		return str_repeat( $this->getOneSymbol( $orderOfMagnitude ), $digit );
+	}
+
+	protected function getOneSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude][0];
+	}
+
+	protected function getFiveSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude][1];
+	}
+
+	protected function getTenSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude + 1][0];
 	}
 
 }
--- RomanNumberFormatterTest.php
+++ RomanNumberFormatterTest.php
@@ -9,10 +9,108 @@ class RomanNumberFormatterTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue( true );
 	}
 
-	public function testFormatNumberOne() {
+	/**
+	 * @dataProvider simpleNumberProvider
+	 */
+	public function testFormatSimpleNumber( $expected, $simpleNumber ) {
+		$this->assertInputResultsInExpected( $expected, $simpleNumber );
+	}
+
+	protected function assertInputResultsInExpected( $expected, $input ) {
 		$formatter = new RomanNumberFormatter();
-		$result = $formatter->formatNumber( 1 );
+		$result = $formatter->formatNumber( $input );
+
 		$this->assertInternalType( 'string', $result );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function simpleNumberProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'I', 1 );
+		$argLists[] = array( 'V', 5 );
+		$argLists[] = array( 'X', 10 );
+		$argLists[] = array( 'L', 50 );
+		$argLists[] = array( 'C', 100 );
+		$argLists[] = array( 'D', 500 );
+		$argLists[] = array( 'M', 1000 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numberWithRepeatedSymbolsProvider
+	 */
+	public function testFormatNumberTwo( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numberWithRepeatedSymbolsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'II', 2 );
+		$argLists[] = array( 'III', 3 );
+		$argLists[] = array( 'XX', 20 );
+		$argLists[] = array( 'XXX', 30 );
+		$argLists[] = array( 'MM', 2000 );
+		$argLists[] = array( 'MMM', 3000 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithFourProvider
+	 */
+	public function testFormatNumbersWithFour( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithFourProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'IV', 4 );
+		$argLists[] = array( 'XL', 40 );
+		$argLists[] = array( 'CD', 400 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithNineProvider
+	 */
+	public function testFormatNumbersWithNine( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithNineProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'IX', 9 );
+		$argLists[] = array( 'XC', 90 );
+		$argLists[] = array( 'CM', 900 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithNineProvider
+	 */
+	public function testFormatNumbersMultipleDigits( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithMultipleDigitsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'XI', 11 );
+		$argLists[] = array( 'XIV', 14 );
+		$argLists[] = array( 'XIX', 19 );
+		$argLists[] = array( 'XIII', 13 );
+		$argLists[] = array( 'XXXIII', 33 );
+		$argLists[] = array( 'CXCI', 191 );
+		$argLists[] = array( 'MMXMXCVIII', 2998 );
+
+		return $argLists;
 	}
 
 }
